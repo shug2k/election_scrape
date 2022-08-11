398a88df91cecf26f1258c9dbaba73ee14fa0fbf
Shug Ghosh <shug@alphaexplorationco.com>
2022-08-10 17:26:55 -0700

    Add election stopwords

--- wordcloud_from_sites.py
+++ wordcloud_from_sites.py
@@ -1,12 +1,18 @@
 import json
 import os
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 
 SOURCE_FOLDER = 'processed_text'
 
+stopwords = set(STOPWORDS)
+election_stopwords_filename = 'sources/election_stopwords.txt'
+
+with open(election_stopwords_filename) as f_stopwords:
+    for line in f_stopwords:
+        stopwords.add(line.strip('\n'))
+
 all_text = ''
-count = 0
 for filename in os.listdir(SOURCE_FOLDER):
     filename_with_path = SOURCE_FOLDER + '/' + filename
 
@@ -14,7 +20,7 @@ for filename in os.listdir(SOURCE_FOLDER):
         text = f.read()
         all_text += ' ' + text
 
-wordcloud = WordCloud(width=4000, height=3200).generate(all_text)
+wordcloud = WordCloud(width=4000, height=3200, stopwords=stopwords).generate(all_text)
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
